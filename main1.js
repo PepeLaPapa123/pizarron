b9291f99171e9f82192488a2ad807b01ffe681b6
PepeLaPapa123 <emichibonachi@gmail.com>
2024-08-12 15:39:04 -0600

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,67 @@
+var last_position_x, last_position_y;
+
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+color = "black";
+width_of_line = 1;
+
+var width = screen.width;
+
+new_width = width - 70;
+
+new_height = screen.height - 300;
+
+if (width < 992)
+{
+    document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+}
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    mouseEvent = "mousedown";
+}
+
+
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e)
+
+{
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+    if (mouseEvent == "mousedown")
+    {
+
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        console.log("Last position of x and Y coordintates")
+        console.log("x = " + last_position_x + "y = " +last_position_y);
+        ctx.moveTo(last_position_x, last_position_y);
+        console.log("current position of x and y coordinates =")
+        console.log("x = " + current_position_of_mouse_x, "y = " + current_position_of_mouse_y );
+
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.stroke(); 
+
+    
+        last_position_x = current_position_of_mouse_x;
+        last_position_y = current_position_of_mouse_y;
+    }
+
+}
+
+function clearArea()
+
+{
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+}
